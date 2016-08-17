729e02bd6e506eba5fdad7298c1f559844bb71c8
chuong2v <chuong.2v@gmail.com>
2016-08-18 03:23:08 +0700

    cucumber-utils

--- bdd/cucumber-utils.js
+++ bdd/cucumber-utils.js
@@ -1,29 +1,27 @@
-export default class CucumberUtils{
-	constructor(args) {
-		// code
+var setSubDocValue = function(obj, keys, value) {
+	var returnObj = Object.clone(obj);
+	var curSub = returnObj[keys[0]];
+	if (keys.length == 1) {
+		returnObj[keys[0]] = value;
+	} else {
+		try {
+			returnObj[keys[0]] = setSubDocValue(curSub, keys.slice(1), value);
+		} catch (error) {
+			throw new Error("KEY_NOT_FOUND", error);
+		}
 	}
+	return returnObj;
+}
 
-	setSubDocValue(obj, keys, value) {
-		var returnObj = Object.clone(obj);
-		var curSub = returnObj[keys[0]];
-		if (keys.length == 1) {
-			returnObj[keys[0]] = value;
-		} else {
-			try {
-				returnObj[keys[0]] = setSubDocValue(curSub, keys.slice(1), value);
-			} catch (error) {
-				throw new Error("KEY_NOT_FOUND", error);
-			}
-		}
-		return returnObj;
+var validateData = function(data) {
+	if (data == "true" || data == "false") {
+		return data == "true";
 	}
-
-	validateData(data) {
-		if (data == "true" || data == "false") {
-			return data == "true";
-		}
-		if (typeof data == "string" && (data.charAt(0) == '{' || data.charAt(0) == '[')) {
-			return JSON.parse(data);
-		}
+	if (typeof data == "string" && (data.charAt(0) == '{' || data.charAt(0) == '[')) {
+		return JSON.parse(data);
 	}
+}
+module.exports = {
+	setSubDocValue,
+	validateData
 }
